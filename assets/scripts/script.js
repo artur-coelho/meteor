acc293dd3b39ddb37eaa1e22ecad98258bf52326
Artur Coelho <artur.rocha.coelho@gmail.com>
2023-05-07 00:26:25 -0300

    counting number of meteors falling on water

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -7,27 +7,61 @@ const ctx = canvas.getContext('2d');
 
 img.onload = () => {
   ctx.drawImage(img, 0, 0);
-  invert();
-  console.log('white', whitePixels);
-  console.log('red', redPixels);
+  coloredPixelsCounter();
+  meteorsOnWaterCounter();
+  console.log('white', whitePixelsAmount);
+  console.log('red', redPixelsAmount);
+  console.log('blue', bluePixelsAmount);
+  console.log('redPixelsColumnPositions', redPixelsColumnPositions);
+  console.log('bluePixelsColumnPositions', bluePixelsColumnPositions);
+  console.log('meteorsOnWaterAmount', meteorsOnWaterAmount);
 };
 
-let whitePixels = 0;
-let redPixels = 0;
+let whitePixelsAmount = 0;
+let redPixelsAmount = 0;
+let bluePixelsAmount = 0;
 
-const invert = () => {
+const redPixelsColumnPositions = [];
+const bluePixelsColumnPositions = [];
+
+const coloredPixelsCounter = () => {
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
   console.log(imageData);
+
+  const imageWidth = imageData.width;
   const data = imageData.data;
+
+  let firstWaterRow = null;
+
   for (let i = 0; i < data.length; i += 4) {
     if (data[i] === 255 && data[i + 1] === 255 && data[i + 2] === 255) {
-      whitePixels += 1;
+      whitePixelsAmount += 1;
     } else if (data[i] === 255 && data[i + 1] === 0 && data[i + 2] === 0) {
-      redPixels += 1;
-    }
+      redPixelsAmount += 1;
+      let redPixelColumnPosition = (i / 4) % imageWidth;
+      redPixelsColumnPositions.push(redPixelColumnPosition);
+    } else if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 255) {
+      const actualRow = Math.floor(i / 4 / imageWidth);
 
-    // data[i] = 255 - data[i]; // red
-    // data[i + 1] = 255 - data[i + 1]; // green
-    // data[i + 2] = 255 - data[i + 2]; // blue
+      if (firstWaterRow === null) {
+        firstWaterRow = actualRow;
+      }
+
+      if (actualRow === firstWaterRow) {
+        let bluePixelColumnPosition = (i / 4) % imageWidth;
+        bluePixelsColumnPositions.push(bluePixelColumnPosition);
+      }
+    }
   }
+};
+
+let meteorsOnWaterAmount = 0;
+const meteorsOnWaterCounter = () => {
+  redPixelsColumnPositions.map((redColumnPosition) => {
+    bluePixelsColumnPositions.map((blueColumnPosition) => {
+      if (redColumnPosition === blueColumnPosition) {
+        meteorsOnWaterAmount += 1;
+      }
+    });
+  });
 };
