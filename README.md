c4ae50e5cb4edab59d25955ece9d9867382b61a9
Artur Coelho <artur.rocha.coelho@gmail.com>
2023-05-07 23:25:08 -0300

    attempts to resolve item 4 and layout changes

---
+++ README.md
@@ -0,0 +1,23 @@
+## METEOR CHALLENGE
+
+![Sky with stars](./assets/img/meteor_challenge_01.png)
+
+### Tasks:
+
+    1.     Count the number of Stars
+    2.     Count the number of Meteors
+    3.     If the Meteors are falling perpendicularly to the Ground (Water level), count how many will fall on the Water
+    4.     (optional) Find the phrase that is hidden in the dots in the sky.
+        a. HINT 1: 175 Characters
+        b. HINT 2: Most of the last tasks’ code can be reused for this one
+
+Pixel Ref:
+
+    (pure white)    Stars
+    (pure red)      Meteors
+    (pure blue)     Water
+    (pure black)    Ground
+
+## How to run
+
+You can install a vscode extension called "Live server" and run
--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,24 +9,33 @@ img.onload = () => {
   ctx.drawImage(img, 0, 0);
   coloredPixelsCounter();
   meteorsOnWaterCounter();
+  starsOnWaterCounter();
   console.log('white', whitePixelsAmount);
   console.log('red', redPixelsAmount);
-  console.log('blue', bluePixelsAmount);
+
+  console.log('whitePixelsColumnPositions', whitePixelsColumnPositions);
   console.log('redPixelsColumnPositions', redPixelsColumnPositions);
   console.log('bluePixelsColumnPositions', bluePixelsColumnPositions);
   console.log('meteorsOnWaterAmount', meteorsOnWaterAmount);
+  console.log('starsOnWaterAmount', starsOnWaterAmount);
+  console.log(binaryString);
+
+  document.getElementById('starAmount').innerHTML = whitePixelsAmount;
+  document.getElementById('meteorAmount').innerHTML = redPixelsAmount;
+  document.getElementById('meteorsOnWater').innerHTML = meteorsOnWaterAmount;
 };
+
+let binaryString = '';
 
 let whitePixelsAmount = 0;
 let redPixelsAmount = 0;
-let bluePixelsAmount = 0;
 
 const redPixelsColumnPositions = [];
 const bluePixelsColumnPositions = [];
+const whitePixelsColumnPositions = [];
 
 const coloredPixelsCounter = () => {
   const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  console.log(imageData);
 
   const imageWidth = imageData.width;
   const data = imageData.data;
@@ -36,10 +45,14 @@ const coloredPixelsCounter = () => {
   for (let i = 0; i < data.length; i += 4) {
     if (data[i] === 255 && data[i + 1] === 255 && data[i + 2] === 255) {
       whitePixelsAmount += 1;
+      let whitePixelColumnPosition = (i / 4) % imageWidth;
+      whitePixelsColumnPositions.push(whitePixelColumnPosition);
+      binaryString += '0';
     } else if (data[i] === 255 && data[i + 1] === 0 && data[i + 2] === 0) {
       redPixelsAmount += 1;
       let redPixelColumnPosition = (i / 4) % imageWidth;
       redPixelsColumnPositions.push(redPixelColumnPosition);
+      binaryString += '1';
     } else if (data[i] === 0 && data[i + 1] === 0 && data[i + 2] === 255) {
       const actualRow = Math.floor(i / 4 / imageWidth);
 
@@ -65,3 +78,73 @@ const meteorsOnWaterCounter = () => {
     });
   });
 };
+
+let starsOnWaterAmount = 0;
+const starsOnWaterCounter = () => {
+  whitePixelsColumnPositions.map((whiteColumnPosition) => {
+    bluePixelsColumnPositions.map((blueColumnPosition) => {
+      if (whiteColumnPosition === blueColumnPosition) {
+        starsOnWaterAmount += 1;
+      }
+    });
+  });
+};
+
+const original = () => {
+  ctx.drawImage(img, 0, 0);
+};
+
+const invert = () => {
+  ctx.drawImage(img, 0, 0);
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    data[i] = 255 - data[i]; // red
+    data[i + 1] = 255 - data[i + 1]; // green
+    data[i + 2] = 255 - data[i + 2]; // blue
+  }
+  ctx.putImageData(imageData, 0, 0);
+};
+
+const grayscale = () => {
+  ctx.drawImage(img, 0, 0);
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    const avg = (data[i] + data[i + 1] + data[i + 2]) / 3;
+    data[i] = avg; // red
+    data[i + 1] = avg; // green
+    data[i + 2] = avg; // blue
+  }
+  ctx.putImageData(imageData, 0, 0);
+};
+
+const removeBlankSpaces = () => {
+  ctx.drawImage(img, 0, 0);
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+  for (let i = 0; i < data.length; i += 4) {
+    if (data[i] === 2 && data[i + 1] === 119 && data[i + 2] === 189) {
+      data[i] = null; // red
+      data[i + 1] = null; // green
+      data[i + 2] = null; // blue
+    }
+  }
+  ctx.putImageData(imageData, 0, 0);
+};
+
+const inputs = document.querySelectorAll('[name=color]');
+for (const input of inputs) {
+  input.addEventListener('change', (evt) => {
+    switch (evt.target.value) {
+      case 'inverted':
+        return invert();
+      case 'grayscale':
+        return grayscale();
+      case 'removeBlank':
+        return removeBlankSpaces();
+      default:
+        return original();
+    }
+  });
+}
